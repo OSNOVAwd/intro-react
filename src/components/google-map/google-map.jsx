54509cdd6a9a1e85f2e5ddfdb815fafc293f83c8
OSNOVAwd <abdurahmanovmurodzon95@gmail.com>
2024-01-18 19:34:12 +0500

    commit

--- src/components/google-map/google-map.jsx
+++ src/components/google-map/google-map.jsx
@@ -1,43 +1,43 @@
-import React from 'react'
-import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
-// YOUR_API_KEY='AIzaSyBRlV3bMU3xR8K-8mEVZtPAQW0Q2mievlQ'
+// import React from 'react'
+// import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
+// // YOUR_API_KEY='AIzaSyBRlV3bMU3xR8K-8mEVZtPAQW0Q2mievlQ'
 
-const libraries = ['places'];
-const mapContainerStyle = {
-  width: '50vw',
-  height: '50vh',
-};
-const center = {
-  lat: 7.2905715, // default latitude
-  lng: 80.6337262, // default longitude
-};
+// const libraries = ['places'];
+// const mapContainerStyle = {
+//   width: '50vw',
+//   height: '50vh',
+// };
+// const center = {
+//   lat: 7.2905715, // default latitude
+//   lng: 80.6337262, // default longitude
+// };
 
-const GoogleMaps = () => {
-  const { isLoaded, loadError } = useLoadScript({
-    googleMapsApiKey: 'AIzaSyBRlV3bMU3xR8K-8mEVZtPAQW0Q2mievlQ',
-    libraries,
-  });
+// const GoogleMaps = () => {
+//   const { isLoaded, loadError } = useLoadScript({
+//     googleMapsApiKey: 'AIzaSyBRlV3bMU3xR8K-8mEVZtPAQW0Q2mievlQ',
+//     libraries,
+//   });
 
-  if (loadError) {
-    return <div>Error loading maps</div>;
-  }
+//   if (loadError) {
+//     return <div>Error loading maps</div>;
+//   }
 
-  if (!isLoaded) {
-    return <div>Loading maps</div>;
-  }
+//   if (!isLoaded) {
+//     return <div>Loading maps</div>;
+//   }
 
-  return (
-    <div>
-      <GoogleMap
-        mapContainerStyle={mapContainerStyle}
-        zoom={10}
-        center={center}
-      >
-        <Marker position={center} />
-      </GoogleMap>
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <GoogleMap
+//         mapContainerStyle={mapContainerStyle}
+//         zoom={10}
+//         center={center}
+//       >
+//         <Marker position={center} />
+//       </GoogleMap>
+//     </div>
+//   );
+// };
 
-export default GoogleMaps
+// export default GoogleMaps
 
